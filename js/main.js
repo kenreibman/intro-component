b44ce49734c4b306bb5df8d00c82498735779080
Ken Reibman <89096442+lmaoken@users.noreply.github.com>
2021-12-02 10:44:24 -0500

    initial

---
+++ js/main.js
@@ -0,0 +1,84 @@
+const form = document.querySelector("form");
+const liveRegion = document.querySelector(".input-aria-live");
+
+
+const testEmail = email=>{
+	let re = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+	return re.test(email);
+}
+
+const setErrors = ( input , errorId ) =>{
+	input.setAttribute("aria-invalid", "true");
+  input.setAttribute("aria-describedBy", errorId);
+  input.classList.add("hasError");
+  input.nextElementSibling.classList.add("show-error");
+}
+
+const removeErrors = input =>{
+	input.removeAttribute("aria-invalid");
+  input.removeAttribute("aria-describedBy");
+  input.classList.remove("hasError");
+  input.nextElementSibling.classList.remove("show-error");
+}
+
+const testInputs = ( input, errorId ) =>{
+
+  if ( input.name === "email") {
+  	if ( !testEmail(input.value) ) return true;
+  }
+  if ( !input.value ) return true;
+  
+  // no errors
+  return false;
+}
+
+/* could be refactor instead of manual checking */
+
+const handleFormSubmit = event =>{
+  event.preventDefault();
+  const errorMessage = [];
+  // This is object destructuring  
+  const { firstName, lastName, email, password } = event.target;
+  
+  // Check if firstName has value or not
+  if ( testInputs(firstName) ) {
+    setErrors(firstName, "errorFirstName");
+    errorMessage.push("firstname");
+  } else {
+    removeErrors(firstName);
+  }
+  
+  // Check if lastname has value or not
+  if ( testInputs(lastName) ) {
+    setErrors(lastName, "errorLastName");
+    errorMessage.push("lastname");  
+  } else {
+    removeErrors(lastName);
+  }
+  
+  // Check if email has value or not
+  if ( testInputs(email) ) {
+    setErrors(email, "errorEmail");
+    errorMessage.push("email");
+  } else {
+    removeErrors(email);
+  }
+
+     //Check if password has value or not
+   if ( testInputs(password) ) {
+     setErrors(password, "errorPassword");
+     errorMessage.push("password");
+   } else {
+     removeErrors(password);
+   }
+  
+  // set the live region if has errrors or not
+  if ( errorMessage ) {
+    liveRegion.textContent = "Form submission invalid. Check your," + errorMessage.join(",") + " input fields"; 
+  } else {
+    liveRegion.textContent = "Form submission success."
+  }
+}
+
+
+form.addEventListener("submit", handleFormSubmit);
